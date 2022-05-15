dc2887096b11a9d5cd25bf2f7446c2db6fb987e0
Hasan <artykovh@gmail.com>
2022-05-15 20:24:48 +0600

    update film models

--- backend/apps/film/models.py
+++ backend/apps/film/models.py
@@ -8,7 +8,15 @@ class Actor(models.Model):
     age = models.IntegerField('Возраст', max_length=3)
     is_alive = models.BooleanField('Жив', default=True)
     biography = models.TextField('Биография')
-    image = models.ImageField('Фото', upload_to='actors_image' )
+    image = models.ImageField('Фото', upload_to='actors_image/' )
+
+    class Meta:
+        verbose_name = "Актер"
+        verbose_name_plural = "Актеры"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
 
 
 class Director(models.Model):
@@ -16,7 +24,16 @@ class Director(models.Model):
     age = models.IntegerField('Возраст', max_length=3)
     is_alive = models.BooleanField('Жив', default=True)
     biography = models.TextField('Биография')
-    image = models.ImageField('Фото', upload_to='director_image')
+    image = models.ImageField('Фото', upload_to='director_image/')
+
+    class Meta:
+        verbose_name = "Режиссер"
+        verbose_name_plural = "Режиссеры"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
 
 class Genre(models.Model):
     name = models.CharField('Название жанра', max_length=30, unique=True)
@@ -44,9 +61,9 @@ class Film(models.Model):
     rating_critic = models.DecimalField('Рейтинг критиков', max_digits=2, decimal_places=1)
     rating_viewer = models.DecimalField('Рейтинг зрителей', max_digits=2, decimal_places=1)
     relise_date = models.CharField("Дата выхода", max_length=50)
-    directors = models.ManyToManyField(Director)
-    actors = models.ManyToManyField(Actor)
-    genres = models.ManyToManyField(Genre)
+    directors = models.ManyToManyField(Director,verbose_name='Режиссер',related_name='film_director')
+    actors = models.ManyToManyField(Actor,verbose_name='Актер',related_name='film_actor')
+    genres = models.ManyToManyField(Genre,verbose_name='Жанр',related_name='film_genre')
     country = models.CharField("Страна", max_length=60)
     long_time = models.IntegerField("Длительность", max_length=4)
 
--- backend/apps/news/models.py
+++ backend/apps/news/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-
+class News(models.Model):
+    title = models.CharField('Заголовок', max_length=100)
+    text = models.TextField('Текст')
+    image = models.ImageField('Фото', upload_to='news_image/')
 
 
