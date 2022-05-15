ee97b783570fad4ac1afa3d3a1ff59ee4efe8fe6
Dauren <daurenjenbaev@gmail.com>
2022-05-15 21:11:33 +0600

    add in settings app news, os dirs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     'backend.apps.accounts',
     'backend.apps.film',
+    'backend.apps.news'
 
 ]
 
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'backend.config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,6 +132,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
