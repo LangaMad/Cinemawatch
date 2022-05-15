eb8c6204ccae725d130681f636e487edc5b49da3
Hasan <artykovh@gmail.com>
2022-05-15 20:34:40 +0600

    add trailer models

--- backend/apps/news/models.py
+++ backend/apps/news/models.py
@@ -6,5 +6,13 @@ class News(models.Model):
     title = models.CharField('Заголовок', max_length=100)
     text = models.TextField('Текст')
     image = models.ImageField('Фото', upload_to='news_image/')
+    created = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+
+
+class Trailer(models.Model):
+    title = models.CharField('Название',max_length=100)
+    link = models.URLField('Сcылка', max_length=200)
+
 
 
