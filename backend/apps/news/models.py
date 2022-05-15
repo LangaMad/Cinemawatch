91e5d5eaead7ca2dabd40c2811a9b84be1ff800d
Dauren <daurenjenbaev@gmail.com>
2022-05-15 21:15:31 +0600

    add class Meta for models news,trailers

--- backend/apps/news/models.py
+++ backend/apps/news/models.py
@@ -8,11 +8,18 @@ class News(models.Model):
     image = models.ImageField('Фото', upload_to='news_image/')
     created = models.DateTimeField('Дата публикации', auto_now_add=True)
 
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
 
 
 class Trailer(models.Model):
     title = models.CharField('Название',max_length=100)
     link = models.URLField('Сcылка', max_length=200)
+    class Meta:
+        verbose_name = 'Трейлер'
+        verbose_name_plural = 'Трейлеры'
 
 
 
