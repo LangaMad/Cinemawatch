02e9de3b48427b8d0ab655f3ecfd2777e5e3f2fe
Dauren <daurenjenbaev@gmail.com>
2022-05-15 22:33:17 +0600

    update film-model, add view, urls

--- backend/apps/film/views.py
+++ backend/apps/film/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DetailView
+)
 # Create your views here.
+
+class BasePage (TemplateView):
+    template_name = 'base.html'
