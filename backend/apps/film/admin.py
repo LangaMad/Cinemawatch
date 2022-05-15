c90be14bc66afd43cac72ba8c348fc07d58c5c6d
Hasan <artykovh@gmail.com>
2022-05-15 21:09:22 +0600

    add admin for news,film

--- backend/apps/film/admin.py
+++ backend/apps/film/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = [
+    'name',
+    'age',
+    'is_alive',
+    'biography',
+    'image',
+    'id',
+    ]
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = [
+    'name',
+    'age',
+    'is_alive',
+    'biography',
+    'image',
+    'id',
+    ]
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = [
+    'name',
+    'slug',
+    'id',
+    ]
+    prepopulated_fields = {'slug': ('name',)}
+
+
+
+
--- backend/apps/news/admin.py
+++ backend/apps/news/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = [
+    'title',
+    'text',
+    'image',
+    'created',
+
+    ]
+
+@admin.register(Trailer)
+class TailerAdmin(admin.ModelAdmin):
+    list_display = [
+    'title',
+    'link',
+
+    ]
